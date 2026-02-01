62f2244a1e2000d542373aa4bb8097a7d1be4f37
dotenv213 <backendguy_2.10.25@proton.me>
2026-02-01 22:56:48 +0330

    feat(userstore): add user entity

---
+++ internal/userstore/model.go
@@ -0,0 +1,11 @@
+package userstore
+
+import "time"
+
+// User data across the module
+type User struct {
+	ID        int64     `json:"id"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
